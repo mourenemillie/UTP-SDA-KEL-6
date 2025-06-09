7ce89ba83b3e0375597749e63ed27bcf892a39df
Haruesaki <ahmaddonijalaludin@gmail.com>
2025-06-09 08:53:48 +0700

    initialize commit

---
+++ UTP_SDA_KEL_6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <ctime>
+#include <map>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
